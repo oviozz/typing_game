cd1a56a2436359bda57e38fa60fd99d9c53e1cb7
oviozz <42685801+oviozz@users.noreply.github.com>
2022-12-27 13:59:47 -0800

    Update typing_functions.py

--- typing_functions.py
+++ typing_functions.py
@@ -7,20 +7,12 @@ import os
 class Paragraph:
     @staticmethod
     def file_open():
-        with open(resource_path('paragraphs.txt')) as paragraph:
+        with open('paragraphs.txt') as paragraph:
             paragraphs = paragraph.readlines()
         return paragraphs[random.randint(0,len(paragraphs)-2)].strip()
 
 
-def resource_path(relative_path):
-    """ Get absolute path to resource, works for dev and for PyInstaller """
-    try:
-    # PyInstaller creates a temp folder and stores path in _MEIPASS
-        base_path = sys._MEIPASS
-    except Exception:
-        base_path = os.path.abspath(".")
 
-    return os.path.join(base_path, relative_path)
 
 
 
